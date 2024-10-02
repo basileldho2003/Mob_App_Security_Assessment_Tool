aac754b3bdb9794c8d6c0c0ec57f3f9f5304dbe2
Basil Eldho <basileldho2003@gmail.com>
2024-10-02 14:14:51 +0530

    Modified config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ def decrypt_password(encrypted_password, key):
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    USERNAME = ''
-    PASSWORD = ''
-    KEY = ''
+    USERNAME = 'basilsvm'
+    PASSWORD = 'gAAAAABm_QVJd6SMRh7k-h4J3GL456URfX2fQ8PDlgPN12gys0EZh9TdRQlNRTQ_7AMcWrC2UX89wemVkCwo7psaTfLzZJxtZQ=='
+    KEY = 'pQeD_Ddd-hShxUs7JgnXen6sb48UHEUIefH5a1o37C0='
     DPASSWORD = decrypt_password(PASSWORD, KEY)
     SQLALCHEMY_DATABASE_URI = f'mysql+mariadb://{USERNAME}:{PASSWORD}@localhost/mobile_security_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
