a3f244f47402769a3ca607874ad575656ab43031
Basil Eldho <basileldho2003@gmail.com>
2024-10-03 14:53:50 +0530

    Create from scratch

--- app/utils/decompiler.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-
-def decompile_apk(apk_file_path):
-    output_dir = os.path.splitext(apk_file_path)[0] + '_decompiled'
-    command = f"apktool d {apk_file_path} -o {output_dir}"
-    subprocess.run(command, shell=True)
-    return output_dir
