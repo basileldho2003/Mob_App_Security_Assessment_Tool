a6d5f5e107cf03b89093deff7f61273efba6f576
Basil Eldho <basileldho2003@gmail.com>
2024-10-08 13:34:09 +0530

    Modified app/__init__.py

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import config_by_name
+
+# Initialize SQLAlchemy
+db = SQLAlchemy()
+
+# Function to create the Flask app
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config_by_name[config_name])
+
+    # Initialize extensions
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    # Register Blueprints (if any)
+    # from .some_module import some_module_blueprint
+    # app.register_blueprint(some_module_blueprint)
+
+    return app
